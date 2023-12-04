a93c89e99f0c33e6a66c8156d43bc37ade4f05c8
Kleby <kleby10@gmail.com>
2023-12-04 00:05:19 -0300

    :sparkles: feat: Adicionado enviroments e ajustando as urls da api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,23 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/Product.interface';
+import { environmentDev } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private readonly api: string = "/assets/mock/db.json";
-  // private readonly api: string = "http://localhost:8080/api";
+  private readonly apiMock: string = "/assets/mock/db.json";
+  private readonly apiDev: string = `${environmentDev.apiUrl}`;
+  private readonly apiUrl: string = `${environment.apiUrl}`
 
   constructor(private http: HttpClient) { }
 
   getAllProducts():Observable<Product[]>{
-    return this.http.get<Product[]>(this.api);
+    return this.http.get<Product[]>(`${this.apiUrl}/product`);
   }
   
   getProductById(id: number | string): Observable<Product>{
-    return this.http.get<Product>(`${this.api}/${id}`)
+    return this.http.get<Product>(`${this.apiUrl}/product/${id}`)
   }
 
   private setPurchaseDate(): Date{
@@ -29,7 +32,7 @@ export class ProductService {
 
   AddProduct(product: Product):Observable<Product>{
     product.purchaseDate = this.setPurchaseDate().toLocaleString('pt-BR'); 
-    return this.http.post<Product>(`${this.api}/new-product`, product);
+    return this.http.post<Product>(`${this.apiUrl}/new-product`, product);
     
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://tepoly-de-teste.onrender.com/api',
+};
